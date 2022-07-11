350ba94440d54bed95dab64221433bd0c2cd305f
Álex <alexfernandodev@gmail.com>
2022-07-11 18:33:57 -0300

    Final Project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,20 @@
 
-var JSONPath = '../../data.json'
-var request = new XMLHttpRequest()
-var data;
 
-request.open('GET', JSONPath)
-request.responseType = 'json'
-request.send()
-request.onload = function() {
-    data = request.response
-    for(let i = 0; i < data.length; i++) {
-        // tests[i].innerHTML = data[i]['amount']
-    }
+const carregarSpend = function() {
+    var JSONPath = '../../data.json'
+
+    fetch(JSONPath).then((response) => {
+        response.json().then((responseData) => {
+            let data = responseData
+            const tooltips = document.querySelectorAll('.card-spend-tooltip')
+            const graphs = document.querySelectorAll('.card-spend-graph')
+            for(let i = 0; i < data.length; i++) {
+                tooltips[i].innerHTML = 'R$' + data[i]['amount']
+                graphs[i].style.height = `${data[i]['amount']}%`
+            }
+        })
+    })
+
 }
+
+document.querySelector('body').addEventListener('load', carregarSpend())

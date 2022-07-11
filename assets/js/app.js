d784d46d7495806f8304a60793feb16a27a26e05
Álex <alexfernandodev@gmail.com>
2022-07-11 18:44:09 -0300

    Final Final Project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,37 @@
+let data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
 
-
-const carregarSpend = function() {
-    var JSONPath = '../../data.json'
-
-    fetch(JSONPath).then((response) => {
-        response.json().then((responseData) => {
-            let data = responseData
-            const tooltips = document.querySelectorAll('.card-spend-tooltip')
-            const graphs = document.querySelectorAll('.card-spend-graph')
-            for(let i = 0; i < data.length; i++) {
-                tooltips[i].innerHTML = 'R$' + data[i]['amount']
-                graphs[i].style.height = `${data[i]['amount']}%`
-            }
-        })
-    })
-
-}
-
-document.querySelector('body').addEventListener('load', carregarSpend())
+  const tooltips = document.querySelectorAll('.card-spend-tooltip')
+  const graphs = document.querySelectorAll('.card-spend-graph')
+  for(let i = 0; i < data.length; i++) {
+      tooltips[i].innerHTML = 'R$' + data[i]['amount']
+      graphs[i].style.height = `${data[i]['amount']}%`
+  }
